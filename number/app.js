6daa5e87c1bafc81b32d3adbfbf3ac713dd82bc7
YulduzRoziqova[FE20] <122364829+YulduzRoziqova@users.noreply.github.com>
2023-06-03 14:52:05 +0500

    Add files via upload

---
+++ number/app.js
@@ -0,0 +1,72 @@
+          // HomeWork numbers
+// function floor(num) {
+//     let res;
+//     if(num > 0){
+//        res = num - ((num * 10) % 10) / 10
+//     }else if(num < 0){
+//         res = num -1 + ((num * 10) % 10) / 10 
+//     }
+//         console.log(res)
+// }
+// floor(12.4)
+
+
+
+// function ceil(num) {
+//     let result;
+//     if(num > 0){
+//        result = num + 1 - ((num * 10) % 10) / 10
+//     }else if(num < 0){
+//         result = num - ((num * 10) % 10) / 10 
+//     }
+//     console.log(result)
+// }
+// ceil(12.4)
+
+
+
+// function round(num) {
+//     let result;
+//     if(((num * 10) % 10) > 5){
+//         result = num + 1 - ((num * 10) % 10) / 10 
+//     }else if(((num * 10) % 10) < 5){
+//         result = num - ((num * 10) % 10) / 10 
+//     }
+//     console.log(result)
+// }
+// round(12,6)
+
+
+
+// function trunc(num) {
+//     let result 
+//     if(num > 0){
+//         result = num - ((num * 10) % 10) / 10
+//     }else if(num < 0){
+//         result = (num - ((num * 10) % 10) / 10) * -1
+//     }
+//     console.log(result)
+// }
+// trunc(-14,5)
+
+
+// function abc(num) {
+//     let result
+//     if(num > 0){
+//         result = num
+//     }else if(num < 0){
+//         result = num * -1
+//     }
+//     console.log(result)
+// }
+// abc(12.9)
+
+
+
+
+
+// function pow(x, y) {
+//     let result = x ** y
+//     console.log(result)
+// }
+// pow(16, 1/2)
