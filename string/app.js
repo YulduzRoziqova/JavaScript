f07d81e316446b4877aaab45080e58dc37ee9fee
YulduzRoziqova[FE20] <122364829+YulduzRoziqova@users.noreply.github.com>
2023-06-03 17:39:56 +0500

    Add files via upload

---
+++ string/app.js
@@ -0,0 +1,60 @@
+    
+    function repeat(str, num) {
+        let result = "";
+        for (let index = 0; index < num; index++) {
+            result = result + str;
+        }
+        return result;
+    }
+
+    console.log(repeat('abcd', 3));
+
+function slice(array, start, end) {
+        let Array =[];
+        if(end===undefined || end > array.length)
+          end = array.length;
+      
+        for (let i = start; i < end; i++) {
+          Array.push(array[i]);
+        }
+        return Array;
+      }
+      
+       console.log(slice(['m', 'a','d','i'],1,3));
+
+
+
+       const indexOf =  function(stringToCheck, position) {
+        let result=[];
+          for (let i = 0; i < stringToCheck.length;i++) {
+            if (stringToCheck[i] === position) {
+              result.push(i);
+            }
+          }
+        console.log(result)
+        }
+        
+        console.log(indexOf('My name is John', 'n'));
+    
+    
+       function subString(num1, num2) {
+       let subString = "";
+        let str = "My firstname is...";
+        let rem = str.length;
+      
+        if (num2 > ren|| num1 < 0 ) {
+          console.log("Invalid input");
+        } else {
+          let ren = 0;
+          for (let i = num1; i < num2; i++) {
+            subString = subString + str[i];
+            ren++;
+          }
+          console.log(subString);
+        }
+      }
+     console.log(subString(0, 7));
+    
+    
+
+        
