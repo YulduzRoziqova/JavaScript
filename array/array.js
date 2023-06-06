bac535290a3b83827333539b7590e367588455d8
YulduzRoziqova[FE20] <122364829+YulduzRoziqova@users.noreply.github.com>
2023-06-06 18:05:55 +0500

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,48 @@
+let array = ['fruit','vegetable','apple','banana','kiwi']
+let result = []
+
+function pop(arr) {
+  return arr[arr.length - 1];
+}
+console.log(pop(array))
+
+function push(arr, newItem){
+arr[arr.length]=newItem
+return arr
+}
+console.log(push(array, 'granat'))
+
+function shift(arr){
+    for (let i = 1; i < arr.length; i++) {
+       result[i - 1] = arr[i]
+    }
+    return result
+}
+console.log(shift(array));
+
+function unshift(arr, newItem) {
+    for (let i = 0; i < arr.length; i++) {
+     result[i+1] = arr[i];
+    }
+    result[0] = newItem
+    return result;
+}
+console.log(unshift(array, 'sam'))
+
+function join(arr = [], item = "") {
+    for (let i = 0; i < arr.length; i++) {
+       item = (i === arr.length - 1) ? "" : item
+       result += arr[i] + item
+    }
+    return result
+ }
+ console.log(join(array))
+
+ 
+ function split(arr, item){
+    for(let i = 0; i < arr.length;i++){
+       result += arr[i] + item
+    }
+    return `[${result}]`
+  }
+  console.log(split('olma tarvuz quvun', ','))
